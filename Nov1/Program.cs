3cc6c00d07c2ddeb6ccb2df9776bfab3a27f9660
pranali-p <pranali.pardhi@lntinfotech.com>
2021-11-01 11:30:18 +0530

    Commit2

--- Nov1/Program.cs
+++ Nov1/Program.cs
@@ -7,7 +7,7 @@ namespace Nov1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Amit");
+            Console.WriteLine("Welcome to Git");
             Console.ReadLine();
         }
     }
